8352b1c0f804de859f13e89f237196f78d53e76a
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-04-11 14:19:30 -0400

    Contacts interface completed and ContactsImplement.java implementation started

--- src/implementationsDao/ContactsImplement.java
+++ src/implementationsDao/ContactsImplement.java
@@ -1,4 +1,27 @@
 package implementationsDao;
 
-public class ContactsImplement {
+
+import Objects.Contact;
+import interfacesDao.ContactsInterface;
+import javafx.collections.ObservableList;
+
+
+public class ContactsImplement implements ContactsInterface {
+
+    ObservableList<Contact> getAllContacts = ContactsInterface.getAllContacts();
+
+    @Override
+    public void updateContact() {
+
+    }
+
+    @Override
+    public void deleteContact() {
+
+    }
+
+    @Override
+    public void addContact() {
+
+    }
 }
--- src/interfacesDao/ContactsInterface.java
+++ src/interfacesDao/ContactsInterface.java
@@ -1,4 +1,21 @@
 package interfacesDao;
 
+import Objects.Contact;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public interface ContactsInterface {
+    ObservableList<Contact> allContacts = FXCollections.observableArrayList();
+
+    static ObservableList<Contact> getAllContacts() {
+        return allContacts;
+    }
+
+
+    public void updateContact();
+
+    public void deleteContact();
+
+    public void addContact();
+
 }
