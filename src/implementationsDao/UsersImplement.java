33c4231a78bdf4769f5b302a07fbe13bbf6630d4
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-04-11 14:37:42 -0400

    Users interface completed and Users implement started

--- src/implementationsDao/UsersImplement.java
+++ src/implementationsDao/UsersImplement.java
@@ -1,4 +1,28 @@
 package implementationsDao;
 
-public class UsersImplement {
+import Objects.Customer;
+import Objects.User;
+import interfacesDao.CustomersInterface;
+import interfacesDao.UsersInterface;
+import javafx.collections.ObservableList;
+
+public class UsersImplement implements UsersInterface {
+
+
+    ObservableList<User> getAllUsers = UsersInterface.getAllUsers();
+
+    @Override
+    public void updateUser() {
+
+    }
+
+    @Override
+    public void deleteUser() {
+
+    }
+
+    @Override
+    public void addUser() {
+
+    }
 }
--- src/interfacesDao/UsersInterface.java
+++ src/interfacesDao/UsersInterface.java
@@ -1,4 +1,22 @@
 package interfacesDao;
 
+import Objects.Customer;
+import Objects.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public interface UsersInterface {
+
+    ObservableList<User> allUsers = FXCollections.observableArrayList();
+
+    static ObservableList<User> getAllUsers() {
+        return allUsers;
+    }
+
+
+    public void updateUser();
+
+    public void deleteUser();
+
+    public void addUser();
 }
