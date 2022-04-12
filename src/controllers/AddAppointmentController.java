c517e9fd972f948b666c50a47c441978c6e8a1eb
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-04-12 17:58:26 -0400

    Set up the add appointment view and began the controller

--- src/controllers/AddAppointmentController.java
+++ src/controllers/AddAppointmentController.java
@@ -1,12 +1,71 @@
 package controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddAppointmentController implements Initializable {
 
+    @FXML
+    private GridPane applicationFormLeft;
+
+    @FXML
+    private TextArea appointmentDescription;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private ComboBox<?> contactComboBox;
+
+    @FXML
+    private TableColumn<?, ?> customerId;
+
+    @FXML
+    private TableColumn<?, ?> customerName;
+
+    @FXML
+    private TextField customerSearch;
+
+    @FXML
+    private TableView<?> customerTable;
+
+    @FXML
+    private TextField locationTxtField;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private DatePicker startDatePicker;
+
+    @FXML
+    private ComboBox<?> startTimeHr;
+
+    @FXML
+    private ComboBox<?> startTimeMin;
+
+    @FXML
+    private TextField titleTxtField;
+
+    @FXML
+    private ComboBox<?> typeComboBox;
+
+    @FXML
+    private TableColumn<?, ?> userId;
+
+    @FXML
+    private TableColumn<?, ?> userName;
+
+    @FXML
+    private TextField userSearch;
+
+    @FXML
+    private TableView<?> userTable;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
