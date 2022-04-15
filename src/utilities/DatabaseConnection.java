29228c77cb731dad8a21f71d6977be4232697d59
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-04-15 02:01:26 -0400

    Beginning the connection in Utilities

--- src/utilities/DatabaseConnection.java
+++ src/utilities/DatabaseConnection.java
@@ -1,13 +1,26 @@
 package utilities;
 
-import java.sql.Driver;
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSetMetaData;
-import java.sql.PreparedStatement;
-import java.sql.CallableStatement;
+import java.sql.*;
 
 public class DatabaseConnection {
+
+
+    String url
+            = "jdbc:derby://localhost:3306/client_schedule";   // JDBC URL
+    String username = "sqlUser";  // Username preset in DB
+    String password = "Passw0rd!";    // Password preset in DB
+
+    Connection conn;  // Get connection
+
+    {
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    // instance of a
+    // Connection
+    // object
+
 }
