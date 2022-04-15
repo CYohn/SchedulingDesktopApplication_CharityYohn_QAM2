4356e3bcd2b68bb30bf242f4c8e4609a828cf1eb
CYohn <cyohn@live.com>
2022-04-14 23:22:14 -0700

    Working on DB connection in the VM

--- src/utilities/DatabaseConnection.java
+++ src/utilities/DatabaseConnection.java
@@ -6,21 +6,36 @@ public class DatabaseConnection {
 
 
     String url
-            = "jdbc:derby://localhost:3306/client_schedule";   // JDBC URL
+            = "jdbc://localhost:3306/client_schedule";   // JDBC URL
     String username = "sqlUser";  // Username preset in DB
     String password = "Passw0rd!";    // Password preset in DB
 
-    Connection conn;  // Get connection
+    Connection connection;  // Get connection
 
     {
         try {
-            conn = DriverManager.getConnection(url, username, password);
-        } catch (SQLException e) {
-            e.printStackTrace();
+            connection = DriverManager.getConnection(url, username, password);
+            Statement stmt = connection.createStatement();       // Create Statement
+            String query = "SELECT * FROM appointments";
+            ResultSet rs = stmt.executeQuery(query);       // Execute Query
+            while (rs.next()) {                            // Process Results
+                System.out.println(rs.getInt("Appointment_ID") + "  ");   // Print Columns
+                System.out.println(rs.getString("Title") + "  ");
+                System.out.println(rs.getString("Description") + "  ");
+                System.out.println(rs.getString("Location") + "  ");
+                System.out.println(rs.getString("Type") + "  ");
+                System.out.println(rs.getString("Start") + "  ");
+                System.out.println(rs.getString("End") + "  ");
+                System.out.println(rs.getString("Customer_ID") + "  ");
+                System.out.println(rs.getString("User_ID") + "  ");
+                System.out.println(rs.getString("Contact_ID") + "  ");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-    }
-    // instance of a
-    // Connection
-    // object
+        // instance of a
+        // Connection
+        // object
 
+    }
 }
