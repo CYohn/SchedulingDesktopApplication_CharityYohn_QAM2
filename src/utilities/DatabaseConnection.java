b1c12fda65d8e4d0db0030432aa0952695311ded
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-04-23 13:38:25 -0400

    Beginning the connection in Utilities

--- src/utilities/DatabaseConnection.java
+++ src/utilities/DatabaseConnection.java
@@ -15,23 +15,23 @@ public class DatabaseConnection {
     {
         try {
             connection = DriverManager.getConnection(url, username, password);
-            Statement stmt = connection.createStatement();       // Create Statement
+            Statement statement = connection.createStatement();       // Create Statement
             String query = "SELECT * FROM appointments";
-            ResultSet rs = stmt.executeQuery(query);       // Execute Query
-            while (rs.next()) {                            // Process Results
-                System.out.println(rs.getInt("Appointment_ID") + "  ");   // Print Columns
-                System.out.println(rs.getString("Title") + "  ");
-                System.out.println(rs.getString("Description") + "  ");
-                System.out.println(rs.getString("Location") + "  ");
-                System.out.println(rs.getString("Type") + "  ");
-                System.out.println(rs.getString("Start") + "  ");
-                System.out.println(rs.getString("End") + "  ");
-                System.out.println(rs.getString("Customer_ID") + "  ");
-                System.out.println(rs.getString("User_ID") + "  ");
-                System.out.println(rs.getString("Contact_ID") + "  ");
+            ResultSet resultSet = statement.executeQuery(query);       // Execute Query
+            while (resultSet.next()) {                            // Process Results
+                System.out.println(resultSet.getInt("Appointment_ID") + "  ");   // Print Columns
+                System.out.println(resultSet.getString("Title") + "  ");
+                System.out.println(resultSet.getString("Description") + "  ");
+                System.out.println(resultSet.getString("Location") + "  ");
+                System.out.println(resultSet.getString("Type") + "  ");
+                System.out.println(resultSet.getString("Start") + "  ");
+                System.out.println(resultSet.getString("End") + "  ");
+                System.out.println(resultSet.getString("Customer_ID") + "  ");
+                System.out.println(resultSet.getString("User_ID") + "  ");
+                System.out.println(resultSet.getString("Contact_ID") + "  ");
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
         }
         // instance of a
         // Connection
