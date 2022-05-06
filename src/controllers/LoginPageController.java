9fedc2576684dced50303358ee063f9a5375a9bd
LabUser <cyohn@live.com>
2022-05-06 15:03:35 -0700

    The login form is functionally communicating with the DB and validating the user

--- src/controllers/LoginPageController.java
+++ src/controllers/LoginPageController.java
@@ -9,15 +9,19 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import utilities.DatabaseConnection;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LoginPageController implements Initializable {
 
-    public static String password = getPasswordField().getText();
-    public static String userName = getUserNameField().getText();
     Stage stage;
     Parent scene;
 
@@ -28,10 +32,13 @@ public class LoginPageController implements Initializable {
     private Button loginButton;
 
     @FXML
-    private static PasswordField passwordField;
+    private PasswordField passwordField;
+
+
 
     @FXML
-    private static TextField userNameField;
+    private TextField userNameField;
+
 
     @FXML
     private Label incorrectInfoLabel;
@@ -45,14 +52,8 @@ public class LoginPageController implements Initializable {
     @FXML
     private Label locationLabel;
 
+    private static String dbPassword;
 
-    public static PasswordField getPasswordField() {
-        return passwordField;
-    }
-
-    public static TextField getUserNameField() {
-        return userNameField;
-    }
 
     @FXML
     void clearButtonClick(MouseEvent event) {
@@ -60,27 +61,73 @@ public class LoginPageController implements Initializable {
         passwordField.clear();
     }
 
+    static Connection connection = DatabaseConnection.getConnection();
+    static PreparedStatement allUsersPreparedStatement;
+
+    public String getUserPassword(String userName) throws SQLException{
+
+        ResultSet getUserResults;
+        try {
+            String getUserSearchStatement = "SELECT User_Name, Password FROM client_schedule.users WHERE User_Name=" + "'" + userName + "';";
+            PreparedStatement getDivisionsFromDB = DatabaseConnection.getConnection().prepareStatement(getUserSearchStatement);
+            getUserResults = getDivisionsFromDB.executeQuery(getUserSearchStatement);
+
+
+            while (getUserResults.next()) {
+                userName = getUserResults.getString("User_Name");
+                System.out.println("Results from getUserPassword: User_Name from DB: " + userName);
+
+                dbPassword = getUserResults.getString("Password");
+                System.out.println("Results from getUserPassword: Password from DB: " + dbPassword);
+                return dbPassword;
+            }
+
+        } catch (SQLException throwables) {
+            System.out.println("SQLException thrown in getUserPassword(String userName) method in the LoginPageController file");
+            throwables.printStackTrace();
+        }
+        return dbPassword;
+    }
+
+    public boolean validateUser() throws SQLException {
+        String password = passwordField.getText();
+        System.out.println("User entered password in validateUser():" + password);
+        String dbPassword = getUserPassword(userNameField.getText());
+        System.out.println("dbPassword from validateUser()" + dbPassword);
+        if (dbPassword.equals(password)) {
+            System.out.println("validateUser() results returns true");
+            return true;
+        } else {
+            System.out.println("validateUser() results returns false");
+            return false;
+        }
+    }
+
     @FXML
-    void loginButtonClick(MouseEvent event) throws IOException {
-        boolean validationStatus = UsersImplement.validateUser(passwordField.getText());
-        if (validationStatus == true){
-            stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
-            scene = FXMLLoader.load((getClass().getResource("/views/TabbedPaneView.fxml")));
+    void loginButtonClick(MouseEvent event) throws IOException, SQLException {
+
+        boolean validationStatus = validateUser();
+
+        if (validationStatus == true) {
+            stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+            scene = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("/views/TabbedPaneView.fxml"))));
             stage.setScene(new Scene(scene));
             stage.show();
+        } else {
+            incorrectInfoLabel.setVisible(true);
         }
-        else{incorrectInfoLabel.setVisible(true);}
     }
 
 
 
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("Login Form Initialized!");
-        passwordRequiredLabel.setVisible(false);
-        userNameReqLabel.setVisible(false);
-        incorrectInfoLabel.setVisible(false);
+        @Override
+        public void initialize (URL url, ResourceBundle resourceBundle){
+            System.out.println("Login Form Initialized!");
+            passwordRequiredLabel.setVisible(false);
+            userNameReqLabel.setVisible(false);
+            incorrectInfoLabel.setVisible(false);
+        }
     }
-}
+
 
--- src/implementationsDao/UsersImplement.java
+++ src/implementationsDao/UsersImplement.java
@@ -11,11 +11,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UsersImplement implements UsersInterface {
+public class UsersImplement extends DatabaseConnection implements UsersInterface  {
 
-    private static String userName = LoginPageController.password;
-    private static String password = LoginPageController.userName;
-    private static String dbPassword;
 
     ObservableList<User> getAllUsers = UsersInterface.getAllUsers();
 
@@ -36,41 +33,6 @@ public class UsersImplement implements UsersInterface {
         }
     }
 
-
-    public static String getUserPassword(String userName) {
-        ResultSet getUserResults;
-        try {
-
-            String getUserSearchStatement = "SELECT User_Name, Password FROM client_schedule.users WHERE User_Name=" + "'" + userName + "';";
-            PreparedStatement getDivisionsFromDB = DatabaseConnection.getConnection().prepareStatement(getUserSearchStatement);
-            getUserResults = getDivisionsFromDB.executeQuery(getUserSearchStatement);
-
-
-            while (getUserResults.next()) {
-                userName = getUserResults.getString("User_Name");
-                System.out.println("User Name: " + userName);
-
-                dbPassword = getUserResults.getString("Password");
-                System.out.println("Password: " + dbPassword);
-            }
-
-        } catch (SQLException throwables) {
-            System.out.println("SQLException thrown in populateDivisionsList() method in the FirstLevelDivisionImplement file");
-            throwables.printStackTrace();
-        }
-        return dbPassword;
-    }
-
-    public static boolean validateUser(String password){
-        String userPassword = password;
-        String dbPassword = getUserPassword(userName);
-        if(dbPassword == userPassword){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
 
     @Override
     public void updateUser() {
