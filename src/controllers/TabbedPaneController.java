c041bd7b87345a1613dac7f26f9a617fb3039895
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-06-02 02:27:49 -0400

    Completed javadocs

--- src/controllers/TabbedPaneController.java
+++ src/controllers/TabbedPaneController.java
@@ -13,114 +13,40 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class TabbedPaneController implements Initializable {
 
-    //TODO: ADD THE ALERT FOR APPOINTMENTS WITHIN 15 MINUTES TO INITIALIZE
-
-    @FXML
-    private AnchorPane addAppointmentAnchor;
 
     @FXML
     private Tab addAppointmentTab;
 
     @FXML
-    private AnchorPane addCustomerAnchor;
-
-    @FXML
     private Tab addCustomerTab;
-
-    @FXML
-    private AnchorPane modifyAppointmentAnchor;
 
     @FXML
     private Tab modifyAppointmentTab;
 
     @FXML
-    private AnchorPane modifyCustomerAnchor;
-
-    @FXML
     private Tab modifyCustomerTab;
-
-    @FXML
-    private AnchorPane reportsAnchor;
 
     @FXML
     private Tab reportsTab;
 
     @FXML
-    private AnchorPane schedulingAnchor;
-
-    @FXML
     private Tab schedulingTab;
 
 
-//    @FXML
-//    void OnAddCustTabReload(ActionEvent event) {
-//        FXMLLoader loader = new FXMLLoader();
-//        loader = new FXMLLoader();
-//        try {
-//            AnchorPane addCustomerAnchor = loader.load(getClass().getResource("/views/AddCustomerView.fxml"));
-//            addCustomerTab.setContent(addCustomerAnchor);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @FXML
-//    void OnSchedTabReload(ActionEvent event) {
-//        FXMLLoader loader = new FXMLLoader();
-//        loader = new FXMLLoader();
-//        try {
-//            AnchorPane schedulingAnchor = loader.load(getClass().getResource("/views/ScheduleView.fxml"));
-//            schedulingTab.setContent(schedulingAnchor);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @FXML
-//    void onAddAppTabReload(ActionEvent event) {
-//        FXMLLoader loader = new FXMLLoader();
-//        loader = new FXMLLoader();
-//        try {
-//            AnchorPane addAppointmentAnchor = loader.load(getClass().getResource("/views/AddAppointmentView.fxml"));
-//            addAppointmentTab.setContent(addAppointmentAnchor);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @FXML
-//    void onModifyApptReload(ActionEvent event) {
-//        FXMLLoader loader = new FXMLLoader();
-//        loader = new FXMLLoader();
-//        try {
-//            AnchorPane modifyAppointmentAnchor = loader.load(getClass().getResource("/views/ModifyAppointmentView.fxml"));
-//            modifyAppointmentTab.setContent(modifyAppointmentAnchor);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @FXML
-//    void onModifyCustTabReload(ActionEvent event) {
-//        FXMLLoader loader = new FXMLLoader();
-//        loader = new FXMLLoader();
-//        try {
-//            AnchorPane modifyCustomerAnchor = loader.load(getClass().getResource("/views/ModifyCustomerView.fxml"));
-//            modifyCustomerTab.setContent(modifyCustomerAnchor);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-
-
-
+    /**
+     * Initializes the page
+     *
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //Load FXML Tabs
         //Load AddCustomerTab
         AtomicReference<FXMLLoader> loader = new AtomicReference<>(new FXMLLoader());
-
+/**
+ * Loads the scheduling tab
+ */
         //Load SchedulingTab
         try {
             AnchorPane schedulingAnchor = loader.get().load(getClass().getResource("/views/ScheduleView.fxml"));
@@ -130,7 +56,9 @@ public class TabbedPaneController implements Initializable {
             System.out.println("I/O exception ScheduleView not found (Tabbed pane controller fxml loader)");
             e.printStackTrace();
         }
-
+/**
+ * Loads the AddCustomer tab
+ */
         loader.set(new FXMLLoader());
         try {
             AnchorPane addCustomerAnchor = loader.get().load(getClass().getResource("/views/AddCustomerView.fxml"));
@@ -141,7 +69,9 @@ public class TabbedPaneController implements Initializable {
             e.printStackTrace();
         }
 
-        //Load modifyCustomerTab
+        /**
+         * Loads the modify customer tab
+         */
         loader.set(new FXMLLoader());
         try {
             AnchorPane modifyCustomerAnchor = loader.get().load(getClass().getResource("/views/ModifyCustomerView.fxml"));
@@ -152,8 +82,9 @@ public class TabbedPaneController implements Initializable {
             System.out.println("I/O exception ModifyCustomerView not found (Tabbed pane controller fxml loader)");
             e.printStackTrace();
         }
-
-        //Load AddAppointmentTab
+/**
+ * Loads the AddAppointment tab
+ */
         loader.set(new FXMLLoader());
         try {
             AnchorPane addAppointmentAnchor = loader.get().load(getClass().getResource("/views/AddAppointmentView.fxml"));
@@ -164,7 +95,9 @@ public class TabbedPaneController implements Initializable {
             e.printStackTrace();
         }
 
-        //Load modifyAppointmentTab
+        /**
+         * Loads the Modify appointment tab
+         */
         loader.set(new FXMLLoader());
         try {
             AnchorPane modifyAppointmentAnchor = loader.get().load(getClass().getResource("/views/ModifyAppointmentView.fxml"));
@@ -175,7 +108,9 @@ public class TabbedPaneController implements Initializable {
             e.printStackTrace();
         }
 
-        //Load reportsTab
+        /**
+         * Loads the reports tab
+         */
         loader.set(new FXMLLoader());
         try {
             AnchorPane reportsAnchor = loader.get().load(getClass().getResource("/views/ReportsView.fxml"));
@@ -187,7 +122,9 @@ public class TabbedPaneController implements Initializable {
         }
 
 
-        //Change listeners to reload the page on reselection
+        /**
+         * Change listener which reloads / refreshes the page on reselection
+         */
         schedulingTab.setOnSelectionChanged(t -> {
             if (schedulingTab.isSelected()) {
                 System.out.println(schedulingTab.getText() + " is selected");
@@ -205,7 +142,9 @@ public class TabbedPaneController implements Initializable {
                 }
             }
         });
-
+        /**
+         * Change listener which reloads / refreshes the page on reselection
+         */
         addCustomerTab.setOnSelectionChanged(t -> {
             if (addCustomerTab.isSelected()) {
                 System.out.println(addCustomerTab.getText() + " is selected");
@@ -223,7 +162,9 @@ public class TabbedPaneController implements Initializable {
                 }
             }
         });
-
+        /**
+         * Change listener which reloads / refreshes the page on reselection
+         */
         modifyCustomerTab.setOnSelectionChanged(t -> {
             if (modifyCustomerTab.isSelected()) {
                 System.out.println(modifyCustomerTab.getText() + " is selected");
@@ -241,7 +182,9 @@ public class TabbedPaneController implements Initializable {
                 }
             }
         });
-
+        /**
+         * Change listener which reloads / refreshes the page on reselection
+         */
         addAppointmentTab.setOnSelectionChanged(t -> {
             if (addAppointmentTab.isSelected()) {
                 System.out.println(addAppointmentTab.getText() + " is selected");
@@ -259,7 +202,9 @@ public class TabbedPaneController implements Initializable {
                 }
             }
         });
-
+        /**
+         * Change listener which reloads / refreshes the page on reselection
+         */
         modifyAppointmentTab.setOnSelectionChanged(t -> {
             if (modifyAppointmentTab.isSelected()) {
                 System.out.println(modifyAppointmentTab.getText() + " is selected");
@@ -277,7 +222,9 @@ public class TabbedPaneController implements Initializable {
                 }
             }
         });
-
+        /**
+         * Change listener which reloads / refreshes the page on reselection
+         */
         reportsTab.setOnSelectionChanged(t -> {
             if (reportsTab.isSelected()) {
                 System.out.println(reportsTab.getText() + " is selected");
