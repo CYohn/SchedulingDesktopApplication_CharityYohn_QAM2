86a690968bdc2017862028fbe21121800eaa1521
Charity Yohn <53451923+CYohn@users.noreply.github.com>
2022-06-04 02:59:27 -0400

    Javadocs done for the utilities.

--- src/utilities/DatabaseConnection.java
+++ src/utilities/DatabaseConnection.java
@@ -4,18 +4,54 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+/**
+ * This class handles the database connection.
+ */
 public class DatabaseConnection {
+    /**
+     * The database protocol.
+     */
     private static final String protocol = "jdbc";
+    /**
+     * The database vendor.
+     */
     private static final String vendor = ":mysql:";
+    /**
+     * The database location.
+     */
     private static final String location = "//localhost/";
+    /**
+     * The database name.
+     */
     private static final String databaseName = "client_schedule";
+    /**
+     * The database URL
+     */
     private static final String jdbcUrl = protocol + vendor + location + databaseName; // LOCAL
+    /**
+     * The driver reference.
+     */
     private static final String driver = "com.mysql.cj.jdbc.Driver"; // Driver reference
+    /**
+     * The database username
+     */
     private static final String userName = "sqlUser"; // Username
-    private static String password = "Passw0rd!"; // Password
+    /**
+     * The database connection
+     */
     public static Connection connection = null;  // Connection Interface
+    /**
+     * The database password
+     */
+    private static String password = "Passw0rd!"; // Password
+    /**
+     * Database prepared statement.
+     */
     private static PreparedStatement preparedStatement;
 
+    /**
+     * This method makes the database connection.
+     */
     public static void makeConnection() {
 
         try {
@@ -23,24 +59,31 @@ public class DatabaseConnection {
             //password = Details.getPassword(); // Assign password
             connection = DriverManager.getConnection(jdbcUrl, userName, password); // reference Connection object
             System.out.println("Connection successful!");
-        }
-        catch(ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             System.out.println("Error:" + e.getMessage());
             e.getCause();
             e.printStackTrace();
-        }
-        catch(SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Error:" + e.getMessage());
             e.getCause();
             e.printStackTrace();
         }
     }
 
+    /**
+     * Gets the database connection.
+     *
+     * @return The database connection.
+     */
     public static Connection getConnection() {
         return connection;
     }
 
-
+    /**
+     * Closes the database connection.
+     *
+     * @return null
+     */
     public static Connection closeConnection() {
         try {
             connection.close();
@@ -53,6 +96,14 @@ public class DatabaseConnection {
         return null;
     }
 
+    /**
+     * The constructs a prepared statement.
+     *
+     * @param sqlStatement A string that will hold an SQL query.
+     * @param conn         The database connection
+     * @return Returns the database response to the prepared statement.
+     * @throws SQLException An exception that provides information on a database access error or other errors.
+     */
     public static PreparedStatement makePreparedStatement(String sqlStatement, Connection conn) throws SQLException {
         if (conn != null)
             preparedStatement = conn.prepareStatement(sqlStatement);
@@ -60,6 +111,7 @@ public class DatabaseConnection {
             System.out.println("Prepared Statement Creation Failed!");
         return null;
     }
+
     public static PreparedStatement getPreparedStatement() throws SQLException {
         if (preparedStatement != null)
             return preparedStatement;
--- src/utilities/TimezoneConversion.java
+++ src/utilities/TimezoneConversion.java
@@ -1,52 +1,64 @@
 package utilities;
 
-import controllers.AddAppointmentController;
-
-import java.sql.Time;
 import java.time.*;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.SimpleTimeZone;
 import java.util.TimeZone;
 
+
+/**
+ * This class handles the time conversions for the application.
+ */
 public class TimezoneConversion {
 
+    /**
+     * The converted time for the business start of the day.
+     */
     private static LocalDateTime businessStartConverted;
+    /**
+     * The converted time for the business end of the day.
+     */
     private static LocalDateTime businessEndConverted;
-    private LocalDateTime appointmentStartOrigin = AddAppointmentController.getStartDate();
-    private LocalDateTime appointmentEndOrigin = AddAppointmentController.getEndDate();
 
-
+    /**
+     * Gets the converted business start time and date.
+     *
+     * @return The converted business start time and date.
+     */
     public static LocalDateTime getBusinessStartConverted() {
         return businessStartConverted;
     }
 
+    /**
+     * Sets the converted business start date and time.
+     *
+     * @param businessStartConverted
+     */
     public static void setBusinessStartConverted(LocalDateTime businessStartConverted) {
         TimezoneConversion.businessStartConverted = businessStartConverted;
     }
 
+    /**
+     * Gets the converted business end time.
+     *
+     * @return The converted business end date and time.
+     */
     public static LocalDateTime getBusinessEndConverted() {
         return businessEndConverted;
     }
 
+    /**
+     * Sets the converted business end date and time.
+     *
+     * @param businessEndConverted
+     */
     public static void setBusinessEndConverted(LocalDateTime businessEndConverted) {
         TimezoneConversion.businessEndConverted = businessEndConverted;
     }
 
-    public LocalDateTime convertAppointmentStartToUTC() {
-        ZonedDateTime startZonedDateTime = appointmentStartOrigin.atZone(ZoneId.of(TimeZone.getDefault().getID()));
-        ZonedDateTime startTimeUTC = startZonedDateTime.withZoneSameInstant(ZoneId.of("UTC"));
-        LocalDateTime appointmentStartUTC = startTimeUTC.toLocalDateTime();
-        return appointmentStartUTC;
-    }
-
-    public LocalDateTime convertAppointmentEndToUTC() {
-        ZonedDateTime endZonedDateTime = appointmentEndOrigin.atZone(ZoneId.of(TimeZone.getDefault().getID()));
-        ZonedDateTime endTimeUTC = endZonedDateTime.withZoneSameInstant(ZoneId.of("UTC"));
-        LocalDateTime appointmentEndUTC = endTimeUTC.toLocalDateTime();
-        return appointmentEndUTC;
-    }
-
+    /**
+     * Converts the business start time from eastern to the user's local time.
+     *
+     * @return The converted date and time.
+     */
     public static LocalDateTime convertBusinessStartFromEstToLocalTime() {
         //create a ZonedDatetime for today at 8:00am EST converted to UTC
         ZoneId estZoneId = ZoneId.of("America/New_York");
@@ -63,6 +75,11 @@ public class TimezoneConversion {
         return userBusinessStartLocal;
     }
 
+    /**
+     * Converts the business end date and time from eastern to the user's local time.
+     *
+     * @return
+     */
     public static LocalDateTime convertBusinessEndFromEstToLocalTime() {
         //create a ZonedDatetime for today at 10:00pm EST converted to UTC
         ZoneId estZoneId = ZoneId.of("America/New_York");
@@ -79,6 +96,12 @@ public class TimezoneConversion {
         return userBusinessEndLocal;
     }
 
+    /**
+     * Converts the user's selected start date and time to UTC for database storage.
+     *
+     * @param userStartDateTime The user's selected start time.
+     * @return Converted start date and time.
+     */
     public static LocalDateTime convertUserStartTimeToUTC(LocalDateTime userStartDateTime) {
         String userTimeZone = TimeZone.getDefault().getID();
         ZoneId userZoneID = ZoneId.of(userTimeZone);
@@ -88,6 +111,12 @@ public class TimezoneConversion {
         return startTimeConvertedToUTC;
     }
 
+    /**
+     * Converts the user's selected end time from eastern to UTC.
+     *
+     * @param userEndDateTime The user's selected end time.
+     * @return
+     */
     public static LocalDateTime convertUserEndTimeToUTC(LocalDateTime userEndDateTime) {
         String userTimeZone = TimeZone.getDefault().getID();
         ZoneId userZoneID = ZoneId.of(userTimeZone);
@@ -97,7 +126,12 @@ public class TimezoneConversion {
         return endTimeConvertedToUTC;
     }
 
-
+    /**
+     * Converts UTC time from the database to the user's local time.
+     *
+     * @param UTCTimeFromDB The UTC time from the database.
+     * @return The converted time and date to the user's local time.
+     */
     public static LocalDateTime convertUTCToUserTime(LocalDateTime UTCTimeFromDB) {
         String userTimeZone = TimeZone.getDefault().getID();
         ZoneId UTC = ZoneId.of("UTC");
@@ -105,7 +139,5 @@ public class TimezoneConversion {
         ZonedDateTime userTimeConverted = timeInUTC.withZoneSameInstant(ZoneId.of(userTimeZone));
         LocalDateTime userLocalDateTime = userTimeConverted.toLocalDateTime();
         return userLocalDateTime;
-
-
     }
 }
